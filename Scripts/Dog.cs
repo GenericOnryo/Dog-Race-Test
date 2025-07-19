b38f1faf9173f2bb9e729b78a78b148413a4404c
Jakethy <101597560+Jakethy@users.noreply.github.com>
2025-07-19 12:17:35 -0700

    Merger and renaming of scripts, plug.
    also plugged in a DogStat field to primary script

--- Scripts/Dog.cs
+++ Scripts/Dog.cs
@@ -1,27 +1,38 @@
-﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Assets.Scripts;
 using UnityEngine;
 
-namespace Assets.Scripts
+public class Dog : MonoBehaviour
 {
-    [CreateAssetMenu(fileName = "New Dog Stat Sheet", menuName = "DRT/Dogs/Stats", order = 100)]
-    public class Dog : ScriptableObject
+    Vector2 lastVelocity;
+
+    private Rigidbody2D rb;
+    public ScriptableObject Dogstats;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        [Range(-10, 10)] public double Size;
-        [Range(0, 10)] public double Speed;
-        [Range(0, 10)] public double Friendliness;
-        [Range(0, 10)] public double Aggression;
-        [Range(0, 10)] public double Intelligence;
-        public GameObject DogGameObject;
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Assets.Scripts.DogVectors.GetRandomVector2(13f);
+        
+    }
 
-        public string Name => DogGameObject?.name ?? "NO PREFAB ASSIGNED";
+    // Update is called once per frame
+    void FixedUpdate()
+    {
 
-        public Dog()
-        {
+        var rb = GetComponent<Rigidbody2D>();
+        lastVelocity = rb.velocity;
+    }
 
-        }
+    public void OnCollisionEnter2D(Collision2D coll)
+    {
+        //  var direction = Vector2.Reflect(lastVelocity.normalized, coll.contacts[0].normal);
+        var collideNormal = coll.GetContact(0).normal;
+        var rb = GetComponent<Rigidbody2D>();
+        var speed = lastVelocity.magnitude;
+        var direction = DogVectors.GetRandomVector2RestrictByNormal(13f, collideNormal);
+        rb.velocity = direction;
     }
 }
