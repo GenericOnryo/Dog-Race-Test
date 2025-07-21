9d54e21a537c1f8816ce2ac1b6928c7698fec95a
Jakethy <101597560+Jakethy@users.noreply.github.com>
2025-07-21 02:10:08 -0700

    dogs accelerate on bounce
    yet to accelerate in transit

--- Scripts/Dog.cs
+++ Scripts/Dog.cs
@@ -13,22 +13,67 @@ public class Dog : MonoBehaviour
     Vector2 lastVelocity;
 
     private Rigidbody2D rb;
-    public Dogstats Dogstats;
 
-    [Range(0, 10)] public double Speed = 10;
+    [Range(-10, 10)] public double Size;
+    [Range(0, 10)] public double Speed;
+    [Range(0, 10)] public double Friendliness;
+    [Range(0, 10)] public double Aggression;
+    [Range(0, 10)] public double Intelligence;
+    [Range(-2, 2)] public double Mood;
+
+
+    private double Acceleration;
+    private double accelerSize;
+    private double accelerSpeed;
+
+    private double maxSpeed;
+    public double speedCurrent;
+    private float elapsedTimeTraveling;
+    public float duration = 2f;
+    private float startTime;
+    private float elapsedTime;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        startTime = Time.time;
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Assets.Scripts.DogVectors.GetRandomVector2((float)Speed);
+        rb.velocity = Assets.Scripts.DogVectors.GetRandomVector2((float)1);
+
+
+        if (Size < -5)
+            accelerSize = (0.01 * -Size) - 0.05;
+        else
+            accelerSize = 0;
+        if (Speed < 5)
+            accelerSpeed = 0.02 * (Speed - 5);
+        else
+            accelerSpeed = 0;
+        Acceleration = (0.01 * Mood) + accelerSize + accelerSpeed;
+
+        if (Size < 0)
+            maxSpeed = 10 + Mood + (Speed * (-0.1 * Size));
+        else if (Size > 0)
+            maxSpeed = 10 + Mood + (Speed * ((0.1 * Size) + (Size * Acceleration)));
+        else
+            maxSpeed = 10 + Mood;
+
+
     }
 
     void Save()
     {
         var savePath = Path.Combine(Application.dataPath, $"{name}.txt");
         var sb = new StringBuilder();
-        sb.AppendLine($"Speed: {Speed}");
+        sb.AppendLine($"SPED: {Speed}");
+        if (Size < 0)
+            sb.AppendLine($"TINY: {-Size}");
+        else
+            sb.AppendLine($"BULK: {Size}"); ;
+        sb.AppendLine($"FREN: {Friendliness}");
+        sb.AppendLine($"AGRO: {Aggression}");
+        sb.AppendLine($"SMRT: {Intelligence}");
         File.WriteAllText(savePath, sb.ToString());
         //JsonSerializer serializer = new JsonSerializer();
 
@@ -46,13 +91,27 @@ public class Dog : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (_lastSaved.AddSeconds(SaveIntervalInSeconds) <= DateTime.Now)
-        {
-            Save();
-            _lastSaved = DateTime.Now;
-        }
+        //if (_lastSaved.AddSeconds(SaveIntervalInSeconds) <= DateTime.Now)
+        //{
+        //    Save();
+        //    _lastSaved = DateTime.Now;
+        //}
         var rb = GetComponent<Rigidbody2D>();
         lastVelocity = rb.velocity;
+
+        elapsedTime = Time.time - startTime;
+        elapsedTimeTraveling = elapsedTime;
+ 
+        double speedNumerator = (maxSpeed + (0.1 * elapsedTimeTraveling));
+        if (Size < -5)
+            speedCurrent = speedNumerator / (1 + Math.Exp(-(((Math.Abs(Size) - 5) * Acceleration) + 0.1) * (elapsedTimeTraveling - 20 + (10 * ((10 * Acceleration) + (0.1 * Math.Abs(Size)))) - Math.Abs(Speed - 4))));
+        else if (Speed > 5)
+            speedCurrent = speedNumerator / (1 + Math.Exp(-(Acceleration + 0.1) * (elapsedTimeTraveling - 20 + (10 * (10 * Acceleration)))));
+        else
+            speedCurrent = speedNumerator / (1 + Math.Exp(-0.1 * (elapsedTimeTraveling - 20)));
+
+
+
     }
 
     public void OnCollisionEnter2D(Collision2D coll)
@@ -61,7 +120,11 @@ public class Dog : MonoBehaviour
         var collideNormal = coll.GetContact(0).normal;
         var rb = GetComponent<Rigidbody2D>();
         var speed = lastVelocity.magnitude;
-        var direction = DogVectors.GetRandomVector2RestrictByNormal((float)Speed, collideNormal);
+        var direction = DogVectors.GetRandomVector2RestrictByNormal((float)speedCurrent, collideNormal);
         rb.velocity = direction;
+
+        
+
+
     }
 }
